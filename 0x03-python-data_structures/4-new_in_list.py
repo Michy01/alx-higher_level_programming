6abc33bcf57df6642e28228aedb9fd388f7afd0f
Michy01 <mkameni20@aiip.alueducation.com>
2022-08-30 07:14:20 +0000

    Add new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx <0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         mod_list = my_list[:]
-        mod_list[idx] = element 
+        mod_list[idx] = element
         return mod_list
