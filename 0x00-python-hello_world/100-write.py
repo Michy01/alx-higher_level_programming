000ea20858448572f921d3cbc1ac848cd41610e7
Michy01 <mkameni20@aiip.alueducation.com>
2022-08-30 13:16:59 +0000

    Add print string

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-value = 'and that piece of art is useful - Dora Korpar, 2015-10-19'
+value = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 text = repr(value)
 sys.stderr.write(text)
 quit(1)
